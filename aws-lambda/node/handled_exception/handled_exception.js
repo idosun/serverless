56a5718deea2ee34934078d3ab602f9ba0899adc
idosun <ishemesh@sentry.io>
2021-03-15 14:59:18 -0700

    update package

--- aws-lambda/node/handled_exception/handled_exception.js
+++ aws-lambda/node/handled_exception/handled_exception.js
@@ -7,20 +7,31 @@
  **/
 
 // Import the Sentry module.
-const Sentry = require("@sentry/node");
-// Configure the Sentry SDK.
-Sentry.init({
-  dsn: "<your DSN>",
+const Sentry = require("@sentry/serverless");
+
+Sentry.AWSLambda.init({
+  dsn: 'https://b1b587b16c334d40934eb9a1602b211d@o87286.ingest.sentry.io/5522728',
+  tracesSampleRate: 1.0,
+
+  request_bodies: "always"
+});
+
+let env_value = process.env.ENV_VAR; // Fetching value of Environment variable set in Lambda function.
+
+Sentry.configureScope(function (scope) {
+  scope.setLevel("error");
+  scope.setTag("custom_tag", "custom_tag_value");
+  scope.setExtra("extra_context", "extra_context_value");
+  scope.setExtra("ENV_VAR", env_value);
 });
 
 // below is the faulty code, undefinedFun() function is not exist.
 
-exports.handler = function (event, context, callback) {
+exports.handler = Sentry.AWSLambda.wrapHandler(async (event, context, callback) => {
   try {
     undefinedFunCall(); // call undefined function.
   } catch (e) {
     Sentry.captureException(e); // Capture the exception in the Sentry dashboard.
-    Sentry.flush(2000);
   }
 
   const response = {
@@ -28,5 +39,11 @@ exports.handler = function (event, context, callback) {
     body: "Handled exception",
   };
 
-  callback(null, response);
-};
+  callback(null, response); 
+ 
+},{
+  //Sentry reports timeout warning when the function is within 500ms of it's execution time. 
+  captureTimeoutWarning: true,
+  timeoutWarningLimit: 300
+  //flushTimeout: 2500  <-- Sentry holds the thread for up to 2 seconds to report errors.  
+});
--- aws-lambda/node/initialization_error/initialization_error.js
+++ aws-lambda/node/initialization_error/initialization_error.js
@@ -11,20 +11,37 @@
  **/
 
 // Import the Sentry module.
-const Sentry = require("@sentry/node");
+const Sentry = require("@sentry/serverless");
 
 // Configure the Sentry SDK.
 Sentry.init({
-  dsn: "<your DSN>",
+  dsn: 'https://b1b587b16c334d40934eb9a1602b211d@o87286.ingest.sentry.io/5522728',
+  tracesSampleRate: 1.0,
+  request_bodies: "always"
+});
+
+let env_value = process.env.ENV_VAR; // Fetching value of Environment variable set in Lambda function.
+
+Sentry.configureScope(function (scope) {
+  scope.setLevel("fatal");
+  scope.setTag("custom_tag", "custom_tag_value");
+  scope.setExtra("extra_context", "extra_context_value");
+  scope.setExtra("ENV_VAR", env_value);
 });
 
 // calling an non-existing function this is an error before invoking lambda
 // handler
-try {
-  notExistFunction();
-} catch (e) {
-  Sentry.captureException(e);
-  Sentry.flush(2000);
-}
+// try {
+//   notExistFunction();
+// } catch (e) {
+//   Sentry.captureException(e);
+//   Sentry.flush(2000);
+// }
 
-exports.handler = function (event, context, callback) {};
+exports.handler = Sentry.AWSLambda.wrapHandler(async (event, context, callback) => {
+
+  notExistFunction();
+
+});
+
+//exports.handler = function (event, context, callback) {};
